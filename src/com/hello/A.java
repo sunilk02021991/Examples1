7f01fc4a572ce06791953ba0847f27ecf028b07f
SUNIL KUMAR <SUNIL KUMAR@sunil>
2024-09-27 08:54:07 +0530

    second commit

--- src/com/hello/A.java
+++ src/com/hello/A.java
@@ -1,5 +1,13 @@
 package com.hello;
 
-public class A {
-
+public class A 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world");
+		
+		System.out.println("Hello sunil");
+		
+	}
+	
 }
