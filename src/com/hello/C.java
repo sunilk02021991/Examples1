5a7076b32766e71bb0a397093eeefdc2a634ae48
SUNIL KUMAR <SUNIL KUMAR@sunil>
2024-11-28 21:52:11 +0530

    this is my second commit

---
+++ src/com/hello/C.java
@@ -0,0 +1,16 @@
+package com.hello;
+
+public class C 
+{
+	
+	public static void main(String[] args)
+	{
+		String str = "sunil kumar";
+		
+		String[] str1 = str.split(" ");
+		
+		System.out.println(str1[1]+" "+str1[0]);
+		
+	}
+	
+}
